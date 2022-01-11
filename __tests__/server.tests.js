283ccad93a62dcda6020868abe075f605793c10a
Christopher Harden <Harden.Christopher00@gmail.com>
2022-01-10 19:03:31 -0600

    fixed

--- __tests__/server.tests.js
+++ __tests__/server.tests.js
@@ -2,14 +2,14 @@
 
 const server = require('../app');
 const supertest = require('supertest');
-const res = require('express/lib/response');
 const req = supertest(server.app);
 
 describe('Testing my HTTP server', () => {
-    IDBTransaction('Should be able to response to a POST to /message', async () => {
-        let res = await req.post('/message?text=test&author=test');
 
-        expect(res.status).toEqual(200);
-        expect(res.body[0].text)toEqual('test');
-    });
+  It('Should be able to response to a POST to /message', async () => {
+    let res = await req.post('/message?text=test&author=test');
+
+    expect(res.status).toEqual(200);
+    expect(res.body[0].text).toEqual('test');
+  });
 });
